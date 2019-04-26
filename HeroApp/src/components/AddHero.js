0c7770cbff6538781759048e62234cf5e294599a
Jupiter <jupiterbaudot@gmail.com>
2019-04-26 16:02:36 -0700

    Add form to AddHero

--- HeroApp/src/components/AddHero.js
+++ HeroApp/src/components/AddHero.js
@@ -2,6 +2,15 @@ import React, {
     Component
 } from "react";
 import axios from "axios";
+import {
+    Image,
+    Item,
+    Responsive,
+    Segment,
+    Form,
+    Button,
+    TextArea
+} from 'semantic-ui-react'
 
 class AddHero extends Component {
     constructor(props) {
@@ -23,7 +32,47 @@ class AddHero extends Component {
             
             <div>
 
-                <h3> Add Hero Component </h3>
+                <h3> Add A Hero! </h3>
+                < hr / >
+                <Form>
+
+                        
+                    < Form.Group widths = 'equal' >
+
+                    
+                        < Form.Input fluid label = 'Hero Name'
+                        id = "heroName"
+                        type = "text"
+                        className = "form-control"
+                        placeholder = "Hero Name"
+                        name = "heroName" />
+
+                        < Form.Input fluid label = 'First Name'
+                        id = "firstName"
+                        type = "text"
+                        className = "form-control"
+                        placeholder = "First Name"
+                        name = "firstName" / >
+
+                        < Form.Input fluid label = 'Last Name'
+                        id = "lastName"
+                        type = "text"
+                        className = "form-control"
+                        placeholder = "Last Name"
+                        name = "lastName" / >
+
+                        < Form.Input fluid label = 'Favorite Food'
+                        id = "favoriteFood"
+                        type = "text"
+                        className = "form-control"
+                        placeholder = "Favorite Food"
+                        name = "favoriteFood" / >
+
+
+                    </Form.Group>
+
+                </Form>
+
 
             </div>
 
